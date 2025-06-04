3b422137da5243797f8ef49baba9e19e0dd2259c
Czapata24 <cristianzapata1115966@correo.itm.edu.co>
2025-06-03 23:00:38 -0500

    ok

--- Proyecto/HankoSpa.Test/obj/Release/net8.0/HankoSpa.Test.AssemblyInfo.cs
+++ Proyecto/HankoSpa.Test/obj/Release/net8.0/HankoSpa.Test.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HankoSpa.Test")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8503894cd4e49cc22f089daec710c7e86a3fb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HankoSpa.Test")]
